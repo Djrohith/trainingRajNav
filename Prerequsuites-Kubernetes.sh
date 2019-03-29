bfea00687d24dc1d6de3630c925a3642bb2cab3c
Djrohith <30309361+Djrohith@users.noreply.github.com>
2019-03-29 09:35:40 +0530

    Update and rename Rohith Prerequsuites-Kubernetes.sh to Prerequsuites-Kubernetes.sh

--- Prerequsuites-Kubernetes.sh
+++ Prerequsuites-Kubernetes.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
-yum update -y
+yum update -y #update packages
 yum install curl -y
 yum install wget -y
 yum install jq -y
 
-#cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-#[kubernetes]
-#name=Kubernetes
-#baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-#enabled=1
-#gpgcheck=1
-#repo_gpgcheck=1
-#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-#EOF
-
-#cat /etc/yum.repos.d/kubernetes.repo
-#yum update
-#rm -rf /etc/yum.repos.d/kubernetes.repo
-#yum update
-
+#Kubectl install
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /bin/kubectl
 yum install kubectl -y
 
+#kops install
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /bin/kops
